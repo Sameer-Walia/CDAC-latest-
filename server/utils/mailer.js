7cd5ef1d42f8da4f9031923db7b2b1a8ac82d583
Sameer-Walia <sameerwalia13@gmail.com>
2026-09-08 13:24:12 +0530

    Fix Gmail SMTP port

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -5,8 +5,9 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.SMTP_UNAME,
         pass: process.env.SMTP_PASS
